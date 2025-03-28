0e0372b28f106265826d45639f2b62b2a78e5f8e
i2phi <jfiturbe@gmail.com>
2025-03-28 11:57:41 -0500

    adding text splitter

--- basicRag/textSplitter.js
+++ basicRag/textSplitter.js
@@ -13,24 +13,24 @@ const supabaseClient = createClient(url, key);
 
 try {
   const result = await fs.readFile("./client-e.txt", "utf-8");
-  const text = await result;
+  const text = result;
 
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
-    chunkOverlap: 50
+    chunkOverlap: 50,
   });
   const output = await splitter.createDocuments([text]);
   console.log(output);
 
   const embeddings = new OpenAIEmbeddings({
     openAIApiKey: OPENAI_KEY,
-    model: "text-embedding-ada-002"
+    model: "text-embedding-ada-002",
   });
 
   await SupabaseVectorStore.fromDocuments(output, embeddings, {
     client: supabaseClient,
     tableName: "documents",
-    queryName: "match_documents"
+    queryName: "match_documents",
   });
 } catch (error) {
   console.error("Detailed error:", error);
